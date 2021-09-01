0410bc0a73040ec9b6b76d4c11419165bbd95714
Prishan Niwanjaya <Prishan.Niwanjaya@axiatadigitallabs.com>
2021-09-01 13:19:28 +0530

    Initial Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+ * @author Prishan
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("........Started AXI sample automation........");
+    }
+}
